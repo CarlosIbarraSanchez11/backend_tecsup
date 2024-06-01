b4b3571292e52a3c38f51831696a939881467249
Carlos <carlosmanuelibarra11@gmail>
2024-06-01 01:03:14 -0500

    Adding ejemplo1.py

---
+++ semana-1/ejemplo1.py
@@ -0,0 +1,25 @@
+# Las clases siemre de debe comenzar con mayusculas
+class Persona:
+    # Constructores __init__ Reservado para todas las clases
+    def __init__(self, nombre, apellido, edad, altura, direccion, peso):
+        # print(nombre, apellido,edad, altura, direccion, peso)
+        
+        # Vamos a agregar propiedades a self
+        # sel.nombre_de_la_propiedad = valor
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        self.altura = altura
+        self.direccion = direccion
+        self.peso = peso
+
+    def saludar(self):
+        return f"Hola me llamo {self.nombre} {self.apellido}, tengo {self.edad} años de edad"
+
+# Para instancia una clase, debemos simplemente llamarla por su nombre y guardarla en una variable
+
+persona1 = Persona("Pepe", "Perez", 29, 1.94, "av La libertad", 80.2)
+print(persona1.saludar())
+
+# 1:06
+
