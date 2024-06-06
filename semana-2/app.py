05e6245548b7f6f27be9dfea6d3bcbaf8d1a6843
Carlos <carlosmanuelibarra11@gmail>
2024-06-05 23:28:42 -0500

    import bcrypt

--- semana-2/app.py
+++ semana-2/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify, request
+import bcrypt
 
 app = Flask(__name__)  # Crear una instancia de la aplicación Flask
+
+users =[]
+
+def encrypt_password(password):
+    # Generar un salt(es un numero aleatorio que se genera y es concatenado al password)
+    # esto se usa por seguridad y evitar ataques de fuerza bruta
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
 
 @app.route('/')
 def home():
@@ -33,29 +42,29 @@ def home():
     #         }
     #     ]
     # }
-
     # return jsonify(datos2)
-    datos ={
-        "celulares":[
-            {
-                "id" : 1,
-                "marca": "apple",
-                "modelo": "iPhone14"
-            },
-            {
-                "id" : 2,
-                "marca": "Samsung",
-                "modelo": "A34"
-            },
-            {
-                "id" : 3,
-                "marca": "Redmi",
-                "modelo": "Nova 11"
-            }
-        ]
-    }
+    return jsonify(users)
+    # datos ={
+    #     "celulares":[
+    #         {
+    #             "id" : 1,
+    #             "marca": "apple",
+    #             "modelo": "iPhone14"
+    #         },
+    #         {
+    #             "id" : 2, 
+    #             "marca": "Samsung",
+    #             "modelo": "A34"
+    #         },
+    #         {
+    #             "id" : 3,
+    #             "marca": "Redmi",
+    #             "modelo": "Nova 11"
+    #         }
+    #     ]
+    # }
 
-    return jsonify(datos)
+    # return jsonify(datos)
     # return jsonify({
     #     "message" : "Hola mundo desde Flask"
     # })
@@ -63,7 +72,10 @@ def home():
 @app.route('/api/v1/user', methods=['POST'])
 def create_user():
     user_data = request.get_json()
-    print(user_data)
+    # user_data['password'] = encrypt_password(request.get_data('password'))
+    user_data['password'] = encrypt_password(user_data.get('password')).decode('utf-8')
+    # print(user_data)
+    users.append(user_data)
     return jsonify({
         "new_user": user_data
     })
