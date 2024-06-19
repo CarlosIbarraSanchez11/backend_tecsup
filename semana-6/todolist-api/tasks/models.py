1a66b8d13aadc1a940ea2ec6aadae3445aa6c7d4
Carlos <carlosmanuelibarra11@gmail>
2024-06-18 23:02:34 -0500

    adding semana-6

---
+++ semana-6/todolist-api/tasks/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(
+        null=True,
+        blank=True,
+        choices=[
+            ('created','creado'),
+            ('in_progress', 'en progreso'),
+            ('finish', 'terminado')
+        ]
+    )
+    color = models.CharField(max_length=50, null= True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'tasks'
+
